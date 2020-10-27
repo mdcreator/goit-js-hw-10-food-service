16cc5e4b62a7579a8b00ebff2824d775ac318a2f
Marina Domashenko <creator.marina@gmail.com>
2020-10-28 00:11:28 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -14,28 +14,40 @@ const Theme = {
 
 // Добавление разметки
 
-const markup = menuItems(menu);
+const markup = createMenuItems(menu);
 refs.menuMarkup.insertAdjacentHTML('afterbegin', markup);
 
+function createMenuItems(menu) {
+  return menuItems(menu);
+}
+
 refs.checkbox.addEventListener('change', onSwitchTheme);
+lightThemeDefault();
+
+function onSwitchTheme() {
+  if (refs.checkbox.cheched) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+
+  //   localStorage.setItem('Theme', e.currentTarget.cheched);
+}
 
 // const lightThemeDefault = localStorage.getItem(cheched) === 'true';
 
-function lightThemeDefault(e) {
-  if (localStorage.getItem(cheched) === 'true') {
+function lightThemeDefault() {
+  const themeDefault = localStorage.getItem('theme');
+  if (themeDefault === Theme.LIGHT) {
     refs.body.classList.add(Theme.LIGHT);
     refs.checkbox.cheched = true;
   } else {
     refs.body.classList.add(Theme.DARK);
-  }
-}
-
-function onSwitchTheme(e) {
-  if (refs.checkbox.cheched) {
-    refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
-  } else {
-    refs.body.classList.add(Theme.LIGHT);
-    refs.body.classList.remove(Theme.DARK);
+    // refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   }
 }
