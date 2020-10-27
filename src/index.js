4d90d494e26dabba6813035dc3dfc1d39628d748
Marina Domashenko <creator.marina@gmail.com>
2020-10-27 14:49:06 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import menu from './menu.json';
 
 const refs = {
   menuMarkup: document.querySelector('.js-menu'),
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
 };
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// Добавление разметки
 
 const markup = menuItems(menu);
 refs.menuMarkup.insertAdjacentHTML('afterbegin', markup);
